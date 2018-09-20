7ed0092f2e3a738f34d34bbfaf60b47eff0dcb48
KK <kongkay@163.com>
2018-09-20 15:56:10 +0800

    project init commit

---
+++ src/main/java/top/kongk/mmall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package top.kongk.mmall.dao;
+
+import top.kongk.mmall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
